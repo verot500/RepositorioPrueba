91f344c62d1901ade2a84c59dd405777532266e7
Veronica <verot500@yahoo.es>
2021-03-25 12:28:30 -0500

    segundo commit

---
+++ inicio.php
@@ -0,0 +1,18 @@
+<html>
+
+<head>
+
+<title>
+Mi Página de prueba
+</title>
+
+</head>
+
+<body>
+<p>
+Bienvenido a mi primera <i>página web</i>.
+<p>
+Espero se sientan a gusto y disfruten su visita.
+</body>
+
+</html>
